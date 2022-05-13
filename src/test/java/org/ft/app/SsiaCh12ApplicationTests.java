93df64b22787da921b4240273f230ff4f3809e78
fteran <fernando.teran@verint.com>
2022-05-13 11:08:26 -0500

    Initial commit

---
+++ src/test/java/org/ft/app/SsiaCh12ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ft.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsiaCh12ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
